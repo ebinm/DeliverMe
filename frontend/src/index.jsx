b5039f0d7481a79ccafefb7b7f71aba9fabe93b7
ge93xax <ge93xax@mytum.de>
2023-05-20 10:48:38 +0200

    Remove TS.

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -7,7 +7,7 @@ import React from "react";
 import {BrowserRouter} from "react-router-dom";
 
 
-const root = createRoot(document.getElementById("root")!)
+const root = createRoot(document.getElementById("root"))
 
 root.render(
     <React.StrictMode>
