ac89dccc58c1eef15adeea510b12334d0d469bc0
ge93xax <ge93xax@mytum.de>
2023-05-22 13:35:36 +0200

    Setup SSL for express.

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,19 @@
 
+const fs = require('fs');
+const https = require('https');
+const privateKey  = fs.readFileSync('./frontend/.cert/deliver.me.key', 'utf8');
+const certificate = fs.readFileSync('./frontend/.cert/deliver.me.crt', 'utf8');
+
 
 const express = require("express")
 const app = express();
 
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8443;
+const credentials = {key: privateKey, cert: certificate};
+
+app.get("/", (req, res) => res.send("Hello There!"))
 
 
-app.listen(PORT, () => {
-    console.log(`Server listening on ${PORT}`);
-});
 
+const httpsServer = https.createServer(credentials, app);
+httpsServer.listen(PORT, () => console.log(`Listening on port: ${PORT}.`));
