dfa66b29355c4f3b92991889c08ba9ef9b5eb2be
Simon Burmer <simon.burmer@sap.com>
2023-05-29 03:46:47 +0200

    Added Modal and Opening hours

--- frontend/src/components/BuyerChooseShop/BuyerChooseShopView.jsx
+++ frontend/src/components/BuyerChooseShop/BuyerChooseShopView.jsx
@@ -2,202 +2,242 @@ import React, { useEffect, useState } from 'react';
 import { List, ListItem, ListItemText } from '@mui/material';
 import { Grid } from '@mui/material';
 import { ListItemButton } from '@mui/material';
-import { FixedSizeList } from 'react-window';
 import { Box } from '@mui/material';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
 import TextField from '@mui/material/TextField';
-import { GoogleMap, LoadScript, Autocomplete, Marker, InfoWindow, OverlayView } from '@react-google-maps/api';
-
+import { GoogleMap, LoadScript, Marker, InfoWindow, } from '@react-google-maps/api';
+import DefineCustomShopModal from './DefineCustomShopModal';
 
 
 const MapWithList = () => {
-    const [inputValue, setInputValue] = React.useState('');
+    const [map, setMap] = useState(null);
+    const [mapCenter, setMapCenter] = useState(null);
+    const [searchValue, setSearchValue] = useState('Munich');
+    const [shops, setShops] = useState([]);
+    const [selectedShop, setSelectedShop] = useState(null);
+    const [showModal, setShowModal] = useState(false);
+    const [CustomShopValues, setCustomShopValues] = useState(null);
+    const [UseCustomShop, setUseCustomShop] = useState(false);
 
-    const containerStyle = {
-        width: '100%',
-        height: '400px'
-    };
+    // getDay returns 0-6, where 0 is Sunday and 6 is Saturday
+    var currentDay = new Date().getDay() - 1;
+    if (currentDay === -1) {
+        currentDay = 6;
+    }
 
-    const center = {
-        lat: 48.137154,
-        lng: 11.576124
-    };
+    useEffect(() => {
+        console.log(CustomShopValues);
+        console.log(UseCustomShop);
+    }, [CustomShopValues, UseCustomShop]);
 
-    const MapWithSearch = () => {
-        const [map, setMap] = useState(null);
-        const [searchValue, setSearchValue] = useState('');
-        const [shops, setShops] = useState([]);
-        const [selectedShop, setSelectedShop] = useState(null);
-        const [mapCenter, setMapCenter] = useState(null);
+    useEffect(() => {
+        if (map) {
+            const defaultCenter = { lat: 48.137154, lng: 11.576124 }
+            map.setCenter(defaultCenter);
+            setMapCenter(defaultCenter);
+        }
+    }, [map]);
 
-        useEffect(() => {
-            if (map) {
-                const placesService = new window.google.maps.places.PlacesService(map);
 
-                const request = {
-                    location: mapCenter ? mapCenter : center,
-                    radius: 1000,
-                    type: 'grocery_or_supermarket'
-                };
+    useEffect(() => {
+        if (map) {
+            const placesService = new window.google.maps.places.PlacesService(map);
+            const request = {
+                location: mapCenter,
+                radius: 4000,
+                type: 'grocery_or_supermarket'
+            };
 
-                placesService.nearbySearch(request, (results, status) => {
-                    if (status === window.google.maps.places.PlacesServiceStatus.OK) {
-                        setShops(results);
-                    }
-                });
-            }
-        }, [map, mapCenter]);
+            placesService.nearbySearch(request, (results, status) => {
+                if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+                    // Fetch operating hours for each shop
+                    const shopPromises = results.map((place) => {
+                        return new Promise((resolve) => {
+                            const detailsRequest = {
+                                placeId: place.place_id,
+                                fields: ['opening_hours']
+                            };
+                            placesService.getDetails(detailsRequest, (placeResult, placeStatus) => {
+                                if (placeStatus === window.google.maps.places.PlacesServiceStatus.OK) {
+                                    // Add operating hours to the place object
+                                    place.opening_hours = placeResult.opening_hours;
+                                }
+                                resolve(place);
+                            });
+                        });
+                    });
 
-        const handlePlaceSelect = () => {
+                    // Wait for all promises to resolve and set the updated shops state
+                    Promise.all(shopPromises).then((updatedShops) => {
+                        setShops(updatedShops);
+                        console.log(updatedShops);
+                    });
+                }
+            });
+        }
+    }, [map, mapCenter]);
+
+    const handlePlaceSelect = () => {
+        console.log('Search Value:', searchValue);
+
+        if (map) {
+            const placesService = new window.google.maps.places.PlacesService(map);
+
+            const request = {
+                input: searchValue,
+                fields: ['geometry']
+            };
+
             console.log('Search Value:', searchValue);
 
-            if (map) {
-                const placesService = new window.google.maps.places.PlacesService(map);
+            const autocompleteService = new window.google.maps.places.AutocompleteService();
+            autocompleteService.getPlacePredictions(request, (predictions, status) => {
+                if (status === window.google.maps.places.PlacesServiceStatus.OK && predictions?.length > 0) {
+                    const placeId = predictions[0].place_id;
+                    placesService.getDetails({ placeId }, (place, status) => {
+                        if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+                            map.setCenter(place.geometry.location);
+                            setMapCenter(place.geometry.location);
+                            console.log(place.geometry.location);
+                        }
+                    });
+                }
+            });
+        }
+    };
 
-                const request = {
-                    input: searchValue,
-                    fields: ['geometry']
-                };
+    const handleInputChange = (event) => {
+        setSearchValue(event.target.value);
+    };
 
-                const autocompleteService = new window.google.maps.places.AutocompleteService();
-                autocompleteService.getPlacePredictions(request, (predictions, status) => {
-                    if (status === window.google.maps.places.PlacesServiceStatus.OK && predictions?.length > 0) {
-                        const placeId = predictions[0].place_id;
-                        placesService.getDetails({ placeId }, (place, status) => {
-                            if (status === window.google.maps.places.PlacesServiceStatus.OK) {
+    const handleInputKeyDown = (event) => {
+        if (event.key === 'Enter') {
+            handlePlaceSelect();
+        }
+    };
 
-                                map.setCenter(place.geometry.location);
-                                setMapCenter(place.geometry.location);
-                                console.log(place.geometry.location);
-                                // Additional logic with selected place information
-                                console.log('Selected Place:', place);
-                            }
-                        });
-                    }
-                });
-            }
-        };
+    const handleListEntryClick = (shop) => {
+        setSelectedShop(shop);
+        // map.setCenter(shop.geometry.location);
+        // setMapCenter(shop.geometry.location);
+    };
 
-        const handleInputChange = (event) => {
-            setSearchValue(event.target.value);
-        };
+    const handleMarkerClick = (shop) => {
+        setSelectedShop(shop);
+    };
 
-        const handleInputKeyDown = (event) => {
-            if (event.key === 'Enter') {
-                handlePlaceSelect();
-            }
-        };
+    const handleOpenModal = () => {
+        setShowModal(true);
+    };
 
-        const handleListEntryClick = (shop) => {
-            setSelectedShop(shop);
-            map.setCenter(shop.geometry.location);
-            setMapCenter(shop.geometry.location);
-        };
-
-
-        const handleMarkerClick = (shop) => {
-            setSelectedShop(shop);
-        };
+    const handleCloseModal = () => {
+        setShowModal(false);
+    };
 
 
-        return (
-            <LoadScript
-            googleMapsApiKey="AIzaSyDtlTfWb_VyQaJfgkmuKG8qqSl0-1Cj_FQ"
-            libraries={["places"]}
-        >
-            <>
-                <Grid container spacing={5}>
-                    <Grid item xs={6} md={4}>
-                        <Typography variant="h5" fontWeight="bold" sx={{ mb: 4 }}>
-                            Select a Shop
-                        </Typography>
+    return (
+        <>
+            <DefineCustomShopModal
+                showModal={showModal}
+                handleCloseModal={handleCloseModal}
+                setUseCustomShop={setUseCustomShop}
+                setCustomShopValues={setCustomShopValues}
+                CustomShopValues={CustomShopValues}
+            />
 
-                    </Grid>
-                    <Grid item xs={6} md={8}>
-                        <Stack
-                            direction={{ xs: 'column', sm: 'row' }}
-                            divider={<Divider orientation="vertical" flexItem />}
-                            spacing={{ xs: 1, sm: 1, md: 1 }}
-                            sx={{ mb: 2 }}
-                        >
-                            <TextField
-                                id="location"
-                                label="Seach Location"
-                                defaultValue={searchValue}
-                                onChange={handleInputChange}
-                                onKeyDown={handleInputKeyDown}
-                                sx={{ width: '100%' }}
-                            />
-                            <Button variant="contained">Seach</Button>
-                        </Stack>
-                    </Grid>
+            <Grid container spacing={5}>
+                <Grid item xs={6} md={4}>
+                    <Typography variant="h5" fontWeight="bold" sx={{ mb: 4 }}>
+                        Select a Shop
+                    </Typography>
                 </Grid>
-                <Grid container spacing={5} sx={{ mb: 4 }}>
-
-                        <Grid item xs={6} md={4}>
-                            <List sx={{ maxHeight: "100%", maxWidth: "100%", overflow: 'auto', }}>
-
-                                {shops
-                                    .filter(shop => shop.types.includes('grocery_or_supermarket')) // Filter by grocery shops
-                                    .map(shop => (
-                                        <ListItem key={shop.place_id} onClick={() => handleListEntryClick(shop)} sx={{ bgcolor: "white", mb: 1, borderRadius: '10px', boxShadow: 3 }}>
-                                            <ListItemButton>
-                                                <ListItemText primary={shop.name} secondary="Jan 7, 2014. When is Ebins next rap concert?" />
-
-                                            </ListItemButton>
-                                        </ListItem>
-                                    ))}
-                            </List>
-
-                        </Grid>
-                        <Grid item xs={6} md={8}>
-                            <Box sx={{ height: '100%', width: '100%' }}>
-                                <GoogleMap
-                                    mapContainerStyle={containerStyle}
-                                    center={center}
-                                    zoom={14}
-                                    onLoad={map => setMap(map)}
-                                >
-                                    {shops.map(shop => (
-                                        <Marker key={shop.place_id} position={shop.geometry.location} onClick={() => handleMarkerClick(shop)}>
-                                            {selectedShop === shop && (
-                                                <InfoWindow>
-                                                    <div>
-                                                        <h3>{shop.name}</h3>
-                                                    </div>
-                                                </InfoWindow>
-                                            )}
-                                        </Marker>
-                                    ))}
-                                </GoogleMap>
-                            </Box>
-                        </Grid>
-
-                </Grid>
-                <Box
-                    sx={{
-                        display: 'flex',
-                        flexDirection: 'row-reverse',
-                    }}
-                >
+                <Grid item xs={6} md={8}>
                     <Stack
                         direction={{ xs: 'column', sm: 'row' }}
                         divider={<Divider orientation="vertical" flexItem />}
                         spacing={{ xs: 1, sm: 1, md: 1 }}
                         sx={{ mb: 2 }}
                     >
-                        <Button variant="contained">Skip</Button>
-                        <Button variant="contained">Select Shop</Button>
+                        <TextField
+                            id="location"
+                            label="Seach Location"
+                            defaultValue={searchValue}
+                            onChange={handleInputChange}
+                            onKeyDown={handleInputKeyDown}
+                            sx={{ width: '100%' }}
+                        />
+                        <Button variant="contained" onClick={handlePlaceSelect}>Search</Button>
                     </Stack>
-                </Box>
-            </>
-            </LoadScript>
+                </Grid>
+            </Grid>
+            <Grid container spacing={5} sx={{ mb: 2 }}>
+                <LoadScript
+                    googleMapsApiKey="AIzaSyDtlTfWb_VyQaJfgkmuKG8qqSl0-1Cj_FQ"
+                    libraries={["places"]}
+                >
+                    <Grid item xs={6} md={4} sx={{ maxHeight: '70vh', overflow: 'auto' }}>
+                        <List >
+                            <ListItem key={0} >
+                                <ListItemButton sx={{ bgcolor: "gray", borderRadius: '10px', boxShadow: 3 }} onClick={() => handleOpenModal()}>
+                                    <ListItemText primary="Input Custom Shop" />
+                                </ListItemButton>
+                            </ListItem>
 
-        );
-    };
-}
+                            {shops
+                                .map(shop => (
+                                    <ListItem key={shop.place_id} >
+                                        <ListItemButton selected={selectedShop === shop} onClick={() => handleListEntryClick(shop)} sx={{ bgcolor: "white", borderRadius: '10px', boxShadow: 3 }} >
+                                            <ListItemText primary={shop.name} secondary={shop.opening_hours.weekday_text[currentDay]} />
+                                        </ListItemButton>
+                                    </ListItem>
+                                ))}
+                        </List>
+                    </Grid>
+                    <Grid item xs={6} md={8}>
+                        <GoogleMap
+                            mapContainerStyle={{
+                                width: '100%',
+                                height: '70vh'
+                            }}
+                            zoom={14}
+                            onLoad={map => setMap(map)}
+                        >
+                            {shops.map(shop => (
+                                <Marker key={shop.place_id} position={shop.geometry.location} onClick={() => handleMarkerClick(shop)}>
+                                    {selectedShop === shop && (
+                                        <InfoWindow>
+                                            <div>
+                                                <h3>{shop.name}</h3>
+                                            </div>
+                                        </InfoWindow>
+                                    )}
+                                </Marker>
+                            ))}
+                        </GoogleMap>
+                    </Grid>
+                </LoadScript>
+            </Grid>
+            <Box
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'row-reverse',
+                }}
+            >
+                <Stack
+                    direction={{ xs: 'column', sm: 'row' }}
+                    divider={<Divider orientation="vertical" flexItem />}
+                    spacing={{ xs: 1, sm: 1, md: 1 }}
+                    sx={{ mb: 2 }}
+                >
+                    <Button variant="contained">Skip</Button>
+                    <Button variant="contained">Select Shop</Button>
+                </Stack>
+            </Box>
+        </>
+    );
+};
 
 export default MapWithList;
---
+++ frontend/src/components/BuyerChooseShop/DefineCustomShopModal.jsx
@@ -0,0 +1,112 @@
+import React from 'react';
+import { Box } from '@mui/material';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import Stack from '@mui/material/Stack';
+import TextField from '@mui/material/TextField';
+import Modal from '@mui/material/Modal';
+import { useTheme } from "@mui/material/styles"
+
+
+const DefineCustomShopModal = ({ showModal, handleCloseModal, setUseCustomShop, setCustomShopValues, CustomShopValues }) => {
+
+    const theme = useTheme()
+
+    const style = {
+        width: "100vh",
+        position: 'absolute',
+        top: '50%',
+        left: '50%',
+        transform: 'translate(-50%, -50%)',
+        bgcolor: theme.palette.primary.main,
+        boxShadow: 24,
+        p: 4,
+        borderRadius: '10px',
+        flexDirection: 'column',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+    };
+
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        setUseCustomShop(true);
+        handleCloseModal();
+    };
+
+    return (
+        <div>
+            <Modal
+                open={showModal}
+                onClose={handleCloseModal}
+                aria-labelledby="modal-modal-title"
+                aria-describedby="modal-modal-description"
+            >
+                <>
+                    <Box sx={style}>
+                        <Typography variant="h4" sx={{ mb: 2 }}>
+                            Custom Shop Form
+                        </Typography>
+                        <Box
+                            sx={{ width: "100%" }}
+                        >
+                            <form onSubmit={handleSubmit}>
+                                <div>
+                                    <TextField
+                                        required
+                                        id="outlined"
+                                        label="Name"
+                                        margin="dense"
+                                        sx={{ width: '100%' }}
+                                        onChange={(e) => setCustomShopValues({ ...CustomShopValues, Name: e.target.value })}
+                                    />
+                                </div>
+                                <div>
+                                    <TextField
+                                        required
+                                        id="filled-error-helper-text"
+                                        label="Street Address"
+                                        margin="dense"
+                                        sx={{ width: '100%' }}
+                                        onChange={(e) => setCustomShopValues({ ...CustomShopValues, Street: e.target.value })}
+                                    />
+                                </div>
+                                <div>
+                                    <TextField
+                                        required
+                                        id="standard-error"
+                                        label="City"
+                                        margin="dense"
+                                        sx={{ width: '100%' }}
+                                        onChange={(e) => setCustomShopValues({ ...CustomShopValues, City: e.target.value })}
+                                    />
+                                </div>
+                                <div>
+                                    <TextField
+                                        required
+                                        id="standard-error"
+                                        label="State"
+                                        margin="dense"
+                                        sx={{ width: '100%' }}
+                                        onChange={(e) => setCustomShopValues({ ...CustomShopValues, State: e.target.value })}
+                                    />
+                                </div>
+                                <Stack
+                                    direction={{ xs: 'column', sm: 'row' }}
+                                    spacing={{ xs: 1, sm: 1, md: 1 }}
+                                    sx={{ mt: 2, justifyContent: 'space-between' }}
+                                >
+                                    <Button variant="contained" onClick={handleCloseModal}>Back</Button>
+                                    <Button variant="contained" type="submit">Select Shop</Button>
+                                </Stack>
+                            </form>
+                        </Box>
+                    </Box>
+                </>
+            </Modal>
+        </div>
+    );
+};
+
+export default DefineCustomShopModal;
