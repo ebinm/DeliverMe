901be0cf2b8c0507da93fd60ba579d087000f592
Simon Burmer <simon.burmer@sap.com>
2023-06-08 11:14:52 +0200

    Added Order List

--- frontend/src/components/ShopperChooseOrder/ShopperChooseOrderView.jsx
+++ frontend/src/components/ShopperChooseOrder/ShopperChooseOrderView.jsx
@@ -1,14 +1,19 @@
-import React, {useEffect, useState} from 'react';
-import {Box, Grid, List, ListItem, ListItemButton, ListItemText} from '@mui/material';
+import React, { useEffect, useState } from 'react';
+import { Box, Grid, List, ListItem, ListItemButton, ListItemText, IconButton, ListItemAvatar } from '@mui/material';
+import Avatar from '@mui/material/Avatar';
+import ImageIcon from '@mui/icons-material/Image';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import Stack from '@mui/material/Stack';
 import Divider from '@mui/material/Divider';
 import TextField from '@mui/material/TextField';
-import {GoogleMap, InfoWindow, LoadScript, Marker,} from '@react-google-maps/api';
+import { GoogleMap, InfoWindow, LoadScript, Marker, } from '@react-google-maps/api';
 import OrderDetailsModal from './OrderDetailsModal';
 import BidOnOrderModal from './BidOnOrderModal';
-
+import { mockedOrders } from '../../util/mockdata';
+import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
 
 const ShopperChooseOrderView = () => {
     const [map, setMap] = useState(null);
@@ -16,6 +21,8 @@ const ShopperChooseOrderView = () => {
     const [showOrderDetailsModal, setShowOrderDetailsModal] = useState(false);
     const [showBidOnOrderModal, setShowBidOnOrderModal] = useState(false);
     const [selectedOrder, setSelectedOrder] = useState(null);
+
+    const [orders] = useState(mockedOrders)
 
 
     useEffect(() => {
@@ -61,9 +68,7 @@ const ShopperChooseOrderView = () => {
 
             <Grid container spacing={5}>
                 <Grid item xs={6} md={4}>
-                    <Typography variant="h5" fontWeight="bold" sx={{ mb: 4 }}>
-                        Select a Shop
-                    </Typography>
+                    <Typography variant="h4" component="h1" sx={{ paddingLeft: '16px' }}>Orders</Typography>
                 </Grid>
                 <Grid item xs={6} md={8}>
                     <Stack
@@ -88,11 +93,47 @@ const ShopperChooseOrderView = () => {
                 >
                     <Grid item xs={6} md={4} sx={{ maxHeight: '70vh', overflow: 'auto' }}>
                         <List >
-                            <ListItem key={0} >
-                                <ListItemButton sx={{ bgcolor: "gray", borderRadius: '10px', boxShadow: 3 }} onClick={() => handleOpenOrderDetailsModal()}>
-                                    <ListItemText primary="Input Custom Shop" />
-                                </ListItemButton>
-                            </ListItem>
+                            {orders.map((order) => (
+                                <ListItem key={order._id}>
+                                    <ListItemButton
+                                        selected={selectedOrder === order}
+                                        onClick={() => {
+                                            setSelectedOrder(order);
+                                        }}
+                                        sx={{ bgcolor: "white", borderRadius: '10px', boxShadow: 3 }}
+                                    >
+                                        <Box sx={{ width: "100%" }}>
+                                            <Stack
+                                                direction={{ xs: 'column', sm: 'row' }}
+                                                spacing={{ xs: 1, sm: 1, md: 1 }}
+                                                sx={{ mb: 2 }}
+                                            >
+                                                <Box display='flex' alignItems='center'>
+                                                    <ListItemAvatar>
+                                                        <Avatar>
+                                                            <ImageIcon />
+                                                        </Avatar>
+                                                    </ListItemAvatar>
+                                                </Box>
+                                                <Box>
+                                                    <Typography variant={"h6"} fontWeight="bold">{order?.createdBy?.firstName}, {order?.createdBy?.lastName}</Typography>
+                                                    <Box display={"grid"} gridTemplateColumns={"min-content auto"} gap={"1px"} >
+                                                        <ShoppingCartOutlinedIcon />
+                                                        <Typography variant={"body1"}>{order?.groceryShop?.name}, {order?.groceryShop?.street}</Typography>
+                                                        <LocationOnOutlinedIcon />
+                                                        <Typography variant={"body1"}>{order?.groceryShop?.city}, {order?.groceryShop?.country}</Typography>
+                                                    </Box>
+                                                </Box>
+                                            </Stack>
+
+                                            <Divider/>
+                                            <Button sx={{ width: '100%', color: 'gray', textTransform: 'none', justifyContent: 'space-between', mt:1, mb:2}} endIcon={<ArrowForwardIosIcon />} onClick={handleOpenOrderDetailsModal}>
+                                                <Typography variant="body1">See more details & place bid</Typography>
+                                            </Button>
+                                        </Box>
+                                    </ListItemButton>
+                                </ListItem>
+                            ))}
                         </List>
                     </Grid>
                     <Grid item xs={6} md={8}>
@@ -108,24 +149,8 @@ const ShopperChooseOrderView = () => {
                     </Grid>
                 </LoadScript>
             </Grid>
-            <Box
-                sx={{
-                    display: 'flex',
-                    flexDirection: 'row-reverse',
-                }}
-            >
-                <Stack
-                    direction={{ xs: 'column', sm: 'row' }}
-                    divider={<Divider orientation="vertical" flexItem />}
-                    spacing={{ xs: 1, sm: 1, md: 1 }}
-                    sx={{ mb: 2 }}
-                >
-                    <Button variant="contained">Skip</Button>
-                    <Button variant="contained">Select Shop</Button>
-                </Stack>
-            </Box>
         </>
     );
 };
 
-export {ShopperChooseOrderView};
+export { ShopperChooseOrderView };
