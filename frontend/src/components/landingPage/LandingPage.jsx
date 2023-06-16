46c7feeecb611e6f26bcdae901ca06ce0e042348
ge35qiq <ebin.madan@tum.de>
2023-06-16 23:52:29 +0200

    merge update landing page

--- frontend/src/components/landingPage/LandingPage.jsx
+++ frontend/src/components/landingPage/LandingPage.jsx
@@ -8,6 +8,7 @@ export default function LandingPage() {
     useExternalScripts("https://cdn.cai.tools.sap/webchat/webchat.js",
         "1864cb48-c651-4fea-91ab-d5de81f561ee", "b1cc896712145b707d9b5e3ac97eafcb", "cai-webchat")
 
+    //All components of the landing page
     return (
         <div style={{display: 'grid'}}>
             <Hero/>
