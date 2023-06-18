22d4adf992fb00b16473c989e3634b64e35c2fbc
ge35qiq <ebin.madan@tum.de>
2023-06-18 18:08:25 +0200

    added api to find all orders shopper bid

--- backend/controllers/orderController.ts
+++ backend/controllers/orderController.ts
@@ -1,82 +1,116 @@
-import { Request, Response } from 'express';
+import {Request, Response} from 'express';
 import {Order, OrderModel} from '../models/order';
+import {Bid} from "../models/bid";
 
 // Controller methods for CRUD operations
-const getAllOrders = async (req: Request, res: Response) => {
+// const getAllOrders = async (req: Request, res: Response) => {
+//     const orders = await OrderModel.find();
+//     res.json(orders);
+// };
+//
+// const getOrderById = async (req: Request, res: Response) => {
+//     try {
+//         const order = await OrderModel.findById(req.params.id);
+//         if (!order) {
+//             return res.status(404).json({error: 'Order not found'});
+//         }
+//         res.json(order);
+//     } catch (error) {
+//         res.status(500).json({error: 'Internal Server Error'});
+//     }
+// };
+//
+// const createOrder = async (req: Request, res: Response) => {
+//     try {
+//         const order = new OrderModel(req.body);
+//         await order.save();
+//         res.status(201).json(order);
+//     } catch (error) {
+//         console.log(error);
+//         res.status(500).json({error: 'Internal Server Error'});
+//     }
+// };
+//
+// const updateOrder = async (req: Request, res: Response) => {
+//     try {
+//         const order = await OrderModel.findByIdAndUpdate(req.params.id, req.body, {
+//             new: true,
+//         });
+//         if (!order) {
+//             return res.status(404).json({error: 'Order not found'});
+//         }
+//         res.json(order);
+//     } catch (error) {
+//         res.status(500).json({error: 'Internal Server Error'});
+//     }
+// };
+//
+// const deleteOrder = async (req: Request, res: Response) => {
+//     try {
+//         const order = await OrderModel.findByIdAndDelete(req.params.id);
+//         if (!order) {
+//             return res.status(404).json({error: 'Order not found'});
+//         }
+//         res.json({message: 'Order deleted successfully'});
+//     } catch (error) {
+//         res.status(500).json({error: 'Internal Server Error'});
+//     }
+// };
+
+export async function getAllOrders(): Promise<Order[]> {
     const orders = await OrderModel.find();
-    res.json(orders);
-};
+    return orders;
+}
 
-const getOrderById = async (req: Request, res: Response) => {
-  try {
-    const order = await OrderModel.findById(req.params.id);
-    if (!order) {
-      return res.status(404).json({ error: 'Order not found' });
-    }
-    res.json(order);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
+export async function getOrderById(id: string): Promise<Order> {
+    const order = await OrderModel.findById(id);
+    return order
+}
 
-const createOrder = async (req: Request, res: Response) => {
-  try {
-    const order = new OrderModel(req.body);
-    await order.save();
-    res.status(201).json(order);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
+export async function createOrder(order: Order) {
+    const orderModel = new OrderModel(order);
+    const savedOrder = await orderModel.save();
+    return savedOrder;
+}
 
-const updateOrder = async (req: Request, res: Response) => {
-  try {
-    const order = await OrderModel.findByIdAndUpdate(req.params.id, req.body, {
-      new: true,
+export async function updateOrder(id: string, order: Order) {
+    const updatedOrder = await OrderModel.findByIdAndUpdate(id, order, {
+        new: true,
     });
-    if (!order) {
-      return res.status(404).json({ error: 'Order not found' });
-    }
-    res.json(order);
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
+    return updatedOrder;
+}
 
-const deleteOrder = async (req: Request, res: Response) => {
-  try {
-    const order = await OrderModel.findByIdAndDelete(req.params.id);
-    if (!order) {
-      return res.status(404).json({ error: 'Order not found' });
-    }
-    res.json({ message: 'Order deleted successfully' });
-  } catch (error) {
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-};
+export async function deleteOrder(id: string) {
+    const deletedOrder = await OrderModel.findByIdAndDelete(id);
+    return deletedOrder;
+}
 
 export async function findOrdersByBuyer(id: number): Promise<Order[]> {
-  const orders = await OrderModel.find()
-      .where("createdBy").equals(id);
-  return orders;
+    const orders = await OrderModel.find()
+        .where("createdBy").equals(id);
+    return orders;
 }
 
 export async function findOrdersByShopper(id: number): Promise<Order[]> {
-  const orders = await OrderModel.find()
-      .where("selectedBid.createdBy").equals(id);
-  return orders;
+    const orders = await OrderModel.find()
+        .where("selectedBid.createdBy").equals(id);
+    return orders;
 }
 
-export async function findBidOrdersByShopper(id: number): Promise<Order[]> {
-  const orders = await OrderModel.find( { bids: { $elemMatch: {createdBy: id } }})
-  return orders;
+export async function findBidOrdersByShopper(id: number): Promise<Order> {
+    const order = await OrderModel.findByIdAndDelete(id);
+    return order;
 }
+//
+// export async function bidOnOrder(id: number, bid: Bid): Promise<Order> {
+//     const order = await OrderModel.findByIdAndDelete(id);
+//     return order;
+// }
 
 export default {
-  getAllOrders,
-  getOrderById,
-  createOrder,
-  updateOrder,
-  deleteOrder,
+    getAllOrders,
+    getOrderById,
+    createOrder,
+    updateOrder,
+    deleteOrder,
 };
--- backend/routes/orderRoutes.ts
+++ backend/routes/orderRoutes.ts
@@ -1,24 +1,65 @@
 import express from 'express';
-import orderController from '../controllers/orderController';
+import orderController, {findOrdersByBuyer, findOrdersByShopper} from '../controllers/orderController';
+import {authenticated, AuthenticatedRequest} from "../middleware/auth";
+import {Schema} from "mongoose";
 
-const 
-router = express.Router();
+const
+    router = express.Router();
 
 
+// router.get('/', async (req, res, next) => {
+//     try {
+//         await orderController.getAllOrders(req, res)
+//     } catch (e) {
+//         console.log(e)
+//         next(e)
+//     }
+// });
+//
+// // TODO: Bring in same form as in buyerRoutes.ts
+// router.get('/:id', orderController.getOrderById);
+// router.post('/', orderController.createOrder);
+// router.put('/:id', orderController.updateOrder);
+// router.delete('/:id', orderController.deleteOrder);
 
-router.get('/', async (req, res, next) => {
+router.get("/", authenticated, async (req: AuthenticatedRequest, res, next) => {
     try {
-        await orderController.getAllOrders(req, res)
+        res.json(await orderController.getAllOrders())
     } catch (e) {
-        console.log(e)
         next(e)
     }
-});
+})
 
-// TODO: Bring in same form as in buyerRoutes.ts
-router.get('/:id', orderController.getOrderById);
-router.post('/', orderController.createOrder);
-router.put('/:id', orderController.updateOrder);
-router.delete('/:id', orderController.deleteOrder);
+router.get("/:id", authenticated, async (req: AuthenticatedRequest, res, next) => {
+    try {
+        res.json(await orderController.getOrderById(req.params.id));
+    } catch (e) {
+        next(e)
+    }
+})
+
+router.post("/", authenticated, async (req: AuthenticatedRequest, res, next) => {
+    try {
+        res.json(await orderController.createOrder(req.body))
+    } catch (e) {
+        next(e)
+    }
+})
+
+router.put("/:id", authenticated, async (req: AuthenticatedRequest, res, next) => {
+    try {
+        res.json(await orderController.updateOrder(req.params.id, req.body))
+    } catch (e) {
+        next(e)
+    }
+})
+
+router.delete("/:id", authenticated, async (req: AuthenticatedRequest, res, next) => {
+    try {
+        res.json(await orderController.deleteOrder(req.params.id))
+    } catch (e) {
+        next(e)
+    }
+})
 
 export default router;
