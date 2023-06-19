8897ea20d4bc2dc3a20fd63a1d88da2eca3a1022
ge93xax <ge93xax@mytum.de>
2023-06-19 23:07:37 +0200

    Intermediate commit.

--- backend/services/notificationService.ts
+++ backend/services/notificationService.ts
@@ -1,40 +1,50 @@
 import io from "socket.io";
 import {DefaultEventsMap} from "socket.io/dist/typed-events";
 import {authenticatedSocket} from "../middleware/auth";
+import {CustomerType, UserNotification} from "../models/customer";
 
 interface NotificationService {
-    notifyBuyerById: (id: number) => void
-    notifyShopperById: (id: number) => void
+    notifyBuyerById: (id: number, notification: UserNotification) => void
+    notifyShopperById: (id: number, notification: UserNotification) => void
 }
 
 export function createNotificationService(server: io.Server): NotificationService {
 
-    const openConnections: Map<number, io.Socket<DefaultEventsMap, DefaultEventsMap>> = new Map()
+    const openConnections: Map<[CustomerType, number], io.Socket<DefaultEventsMap, DefaultEventsMap>> = new Map()
 
     server.use(authenticatedSocket)
 
     server.on('connection', socket => {
+        const {customerType, customerId} = socket.handshake.auth
+        openConnections.set([customerType, customerId] as [CustomerType, number], socket)
 
-        const auth = socket.handshake.auth
-        console.log("Hello")
-        console.log(auth)
-        // console.log("Hello")
-        // console.log(socket.handshake)
-        // console.log(socket.handshake.headers)
-        // console.log(socket.handshake.headers.cookie)
-        // console.log(socket.handshake.headers.cookies?.["jwt"])
-
-
-        socket.on('event', data => { /* … */
+        socket.on('event', () => {
+            // TODO
         });
-        socket.on('disconnect', () => { /* … */
+
+        socket.on('disconnect', () => {
+            openConnections.delete([customerType, customerId])
         });
     });
 
+    function emitNotification(type: CustomerType, id: number, notification: UserNotification) {
+        try {
+            const socket = openConnections.get([type, id])
+            if (socket === undefined) {
+                return
+            }
+            socket.emit("notification", JSON.stringify(notification))
+        } catch {
+            // TODO
+        }
+    }
+
     return {
-        notifyBuyerById: () => {
+        notifyBuyerById: (id, notification) => {
+            emitNotification("BUYER", id, notification)
         },
-        notifyShopperById: () => {
-        },
+        notifyShopperById: (id, notification) => {
+            emitNotification("SHOPPER", id, notification)
+        }
     }
 }
