ef5a71d4cad6721e127c8f69c938a989ec5e2373
Anxhela Maloku <anxhelamaloku@Anxhelas-MacBook-Pro.local>
2023-06-26 23:54:02 +0200

    same purpose but right files, one mistake at the other commit

---
+++ frontend/src/components/payprovider/PayPal.jsx
@@ -0,0 +1,41 @@
+import React, {useState} from 'react';
+import { PayPalScriptProvider, PayPalButtons } from '@paypal/react-paypal-js';
+
+export default function PayPal () {
+  
+  const clientId = process.env.REACT_APP_CLIENT_ID;
+  const [amount, setAmount] = useState('70');
+
+  const handlePaymentSuccess = (data, actions) => {
+    // Handle successful payment
+    console.log('Payment successful:', data);
+    return Promise.resolve(); //check this out!!
+  };
+
+  const handlePaymentError = (err) => {
+    console.log(err);
+  };
+  const createPayPalOrder = (data, actions) => {
+    return actions.order.create({
+      purchase_units: [
+        {
+          amount: {
+            currency_code: 'USD',
+            value: '70',// value: amount
+          },
+        },
+      ],
+    });
+  };
+
+
+  return (
+    <PayPalScriptProvider options={{ 'client-id': clientId }}>
+      <PayPalButtons
+        createOrder={createPayPalOrder}
+        onApprove={handlePaymentSuccess}
+        onError={handlePaymentError}
+      />
+    </PayPalScriptProvider>
+  );
+}
