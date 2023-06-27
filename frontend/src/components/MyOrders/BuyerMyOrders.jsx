d92b9f2b44dcec39f008a9aab1ec2e3bbdc359d1
ge93xax <ge93xax@mytum.de>
2023-06-27 11:29:56 +0200

    Replace create new link with create new button in shopper my orders.

--- frontend/src/components/MyOrders/BuyerMyOrders.jsx
+++ frontend/src/components/MyOrders/BuyerMyOrders.jsx
@@ -1,28 +1,37 @@
-import {NavLink} from "react-router-dom";
 import React from "react";
-import {CircularProgress, Typography} from "@mui/material";
+import {CircularProgress, Fab, Typography} from "@mui/material";
 import {For, Show} from "../util/ControlFlow";
 import {SingleOrderViewBuyer} from "./SingleOrderViewBuyer";
 import {useFetch} from "../../util/hooks";
 import {GuardCustomerType} from "../util/GuardCustomerType";
-
+import AddIcon from '@mui/icons-material/Add';
+import Stack from "@mui/material/Stack";
 
 export function BuyerMyOrders() {
     const [orders, setOrders, loading] = useFetch(`${process.env.REACT_APP_BACKEND}/api/orders`, {credentials: 'include'})
 
 
     return <GuardCustomerType requiredType={"BUYER"} navigateOnInvalidType={"/shopper/my-orders"}>
+        <Stack direction={"column"} sx={{"paddingBottom": "64px"}}>
+            <Typography variant={"h4"} component={"h1"}>My Orders</Typography>
 
-        <Typography variant={"h4"} component={"h1"}>
-            My Orders&nbsp;
-            <NavLink to={"/buyer/order/create"}>[Create new]</NavLink>
-        </Typography>
+            <Show when={loading}>
+                <CircularProgress/>
+            </Show>
+            <For fallback={<Typography>You have not created any orders yet.</Typography>}
+                 each={orders}>{(order, index) =>
+                <SingleOrderViewBuyer setOrders={setOrders} key={order._id} order={order} orderName={`Order ${index}`}/>
+            }</For>
 
-        <Show when={loading}>
-            <CircularProgress/>
-        </Show>
-        <For fallback={<Typography>You have not created any orders yet.</Typography>} each={orders}>{(order, index) =>
-            <SingleOrderViewBuyer setOrders={setOrders} key={order._id} order={order} orderName={`Order ${index}`}/>
-        }</For>
+            <Fab sx={{
+                "color": "white",
+                "bgcolor": "primary.dark",
+                "position": "fixed",
+                "bottom": "32px",
+                "right": "32px",
+            }} aria-label="add" href={"/buyer/order/create"}>
+                <AddIcon/>
+            </Fab>
+        </Stack>
     </GuardCustomerType>
 }
