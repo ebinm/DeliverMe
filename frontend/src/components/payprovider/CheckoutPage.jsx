1a6f191babb8ab4ddd16b2d3382d74de649048f2
Anxhela Maloku <anxhelamaloku@Anxhelas-MacBook-Pro.local>
2023-07-13 18:37:54 +0200

    buttons customization, checkout placement, closed modal, automatically navigated back to orders

--- frontend/src/components/payprovider/CheckoutPage.jsx
+++ frontend/src/components/payprovider/CheckoutPage.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useContext } from 'react';
-import { Box, Container, Typography } from '@mui/material';
-import PaymentForm from './PaymentForm';
+import { Box, Container, Typography, FormControlLabel } from '@mui/material';
+import { styled } from '@mui/system';
+import { FaPaypal } from 'react-icons/fa';
 import {DarkButton, OutlinedButton} from "../util/Buttons";
 import {useSnackbar} from "notistack";
 import {useFetch} from "../../util/hooks";
@@ -8,25 +9,40 @@ import {CustomerContext} from "../../util/context/CustomerContext";
 import {useNavigate } from "react-router-dom";
 import { useParams } from "react-router-dom"; 
 import { BaseModal } from '../util/BaseModal';
-
+import PayPal from './PayPal';
 
 
 
 export  function CheckoutPage() {
   const [open, setOpen] = useState(true);
+  const [isTransactionCompleted, setTransactionCompleted] = useState(false);
+
+  const handleTransactionComplete = () => {
+    setTransactionCompleted(true);
+  };
+
+
   const params = useParams();
 
   const [orders, setOrder] =  useFetch(`${process.env.REACT_APP_BACKEND}/api/orders/${params.id}`, {credentials: 'include'})
   console.log(orders);
+  //const totalAmount = (orders?.selectedBid?.moneyBidWithFee.amount.toFixed(2) + orders.groceryBill.costAmount.toFixed(2));
   
-
+  
   const navigate = useNavigate();
 
   const {enqueueSnackbar} = useSnackbar();
 
   const {customer} = useContext(CustomerContext);
 
-
+  const IconContainer = styled(Box)({
+    display: 'flex',
+    alignItems: 'center',
+  });
+  
+  const Icon = styled('div')(({ theme }) => ({
+    marginLeft: theme.spacing(1),
+  }));
 
   const lineStyle = {
     borderBottom: '1px solid #E2E8F0',
@@ -62,8 +78,8 @@ const handleConfirm = async () => {
   
   
     return <BaseModal
-                open={open} >
-      <Box>
+            open={open && !isTransactionCompleted}>
+        <Box>
           <Container>
               <Box >
                 <Typography component="h1" variant="h5" align="left">
@@ -71,33 +87,44 @@ const handleConfirm = async () => {
                 </Typography>
               </Box>
               <div style={lineStyle} />
-              
+              {orders && orders?.selectedBid && orders.groceryBill && (
               <Box>
-              {orders && orders?.selectedBid &&(
+              
                 <div style={rowStyle}>
                   {/*  total-bill?? */}
                   <Typography variant="body1">Delivery Costs (with fee)</Typography>
-                  <Typography variant="body1">{orders?.selectedBid?.moneyBid.amount} {orders?.selectedBid?.moneyBid.currency}</Typography>
+                  <Typography variant="body1">{orders?.selectedBid?.moneyBidWithFee.amount} {orders?.selectedBid?.moneyBidWithFee.currency}</Typography>
                 </div>
-              )}
-
-                {orders && orders.groceryBill && (
+              
                 <div style={rowStyle}>
                   <Typography variant="body1">Grocery Bill</Typography>
                   <Typography variant="body1">{orders.groceryBill.costAmount}  {orders.groceryBill.costCurrency}</Typography>
                 </div>
-                )}
-                {orders && orders?.selectedBid &&(
+               
                 <div style={rowStyle}>
                   <Typography variant="body1">Total</Typography>
-                  <Typography variant="body1">{orders?.selectedBid?.moneyBidWithFee.amount}  {orders?.selectedBid?.moneyBidWithFee.currency}</Typography>
+                  <Typography variant="body1">{(orders.groceryBill.costAmount + orders?.selectedBid?.moneyBidWithFee.amount).toFixed(2)}  {orders.groceryBill.costCurrency}</Typography>
                 </div>
-                )}
               </Box>
+              )}
               
               
               <div style={lineStyle} />
-              <PaymentForm/>
+              
+              <Box>
+                <Box display="flex" alignItems="center">
+                  <Typography variant="body1">PayPal</Typography>
+                  <IconContainer>
+                    <Icon>
+                      <FaPaypal size={20} />
+                    </Icon>
+                  </IconContainer>
+                </Box>
+            
+              <Box flex={1} display="flex" justifyContent="center" alignItems="center" marginTop={2}>
+                <PayPal onTransactionComplete={handleTransactionComplete} />
+              </Box>
+            </Box>
               <div style={lineStyle} />
               <Box display="flex" flexDirection="row">
                  <OutlinedButton onClick={handleCancel}>Cancel</OutlinedButton>
--- frontend/src/components/payprovider/PayPal.jsx
+++ frontend/src/components/payprovider/PayPal.jsx
@@ -1,14 +1,19 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useState, useContext } from 'react';
 import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import { useSnackbar } from "notistack";
 import { loadScript } from "@paypal/paypal-js";
-import { useParams } from "react-router-dom"; 
+import {CustomerContext} from "../../util/context/CustomerContext";
+import { useParams, useNavigate } from "react-router-dom"; 
 import {useFetch} from "../../util/hooks";
 import { PUT_FETCH_OPTIONS } from "../../util/util";
 
-export default function PayPal() {
+export default function PayPal({ onTransactionComplete }) {
   const { enqueueSnackbar } = useSnackbar();
   const params = useParams();
+  const navigate = useNavigate();
+  const {customer} = useContext(CustomerContext);
+
+
   const [paypalReady, setPaypalReady] = useState(false);
   const [orders, setOrder] =  useFetch(`${process.env.REACT_APP_BACKEND}/api/orders/${params.id}`, {credentials: 'include'})
   console.log(orders);
@@ -41,9 +46,7 @@ export default function PayPal() {
       });
   };
 
-  const formattedAmount = orders?.selectedBid?.moneyBidWithFee.amount.toFixed(2);
-  const fee = orders?.selectedBid?.moneyBidWithFee.amount.toFixed(2);
-
+  
 
   if (!paypalReady) {
     return null; // or render a loading spinner
@@ -54,41 +57,55 @@ export default function PayPal() {
     <PayPalScriptProvider options={{
       "client-id": process.env.CLIENT_ID
     }}>
-    {orders && orders?.selectedBid && (
+    {orders && orders?.selectedBid && orders.groceryBill && (
       <PayPalButtons
         createOrder={(data, actions, err) => {
+          const totalAmount = (orders.groceryBill.costAmount + orders?.selectedBid?.moneyBidWithFee.amount).toFixed(2);
+          const shopperAmount = orders?.selectedBid?.moneyBid.amount.toFixed(2);
+          const fee = (orders?.selectedBid?.moneyBidWithFee.amount - orders?.selectedBid?.moneyBid.amount).toFixed(2);
+
           return actions.order.create({
             intent: 'CAPTURE',
             purchase_units: [
-              {
-                amount: {
-                  value: formattedAmount
-                },
-                payment_instruction: {
-                  platform_fees: [
-                    {
-                      amount: {
-                        currency_code: 'EUR',
-                        value: formattedAmount
-                      },
-                      payee: {
-                        email_address: 'anxhela.maloku@tum.de' //TODO: shopper email, haven't changed due to test issues!!
-                      },
-                    }
-                  ]
-                }
-              }
-            ]
+    {
+      reference_id: "REFID-1",
+      amount: {
+        currency_code: "EUR",
+        value: totalAmount
+      },
+      payment_instruction: {
+        disbursement_mode: "INSTANT",
+        platform_fees: [
+          {
+            amount: {
+              currency_code: "EUR",
+              value: totalAmount
+            },
+            payee: {
+              email_address: "anxhela.maloku@tum.de" // Replace with a valid PayPal sandbox email for the first recipient
+            }
+          }
+        ]
+      }
+    }
+  ]
           });
         }}
 
         onApprove={async (data, actions) => { // Pass 'data' and 'actions' parameters
           return actions.order.capture()
+          
             .then((orderData) => {
+             
               console.log('Capture result', orderData, JSON.stringify(orderData, null, 2));
               const transaction = orderData.purchase_units[0].payments.captures[0];
-              enqueueSnackbar("Transaction ${transaction.status}: ${transaction.id}", { variant: "success"});
+              if (transaction.status === 'COMPLETED') {
+                onTransactionComplete(); // Call the callback function to notify transaction completion
+                navigate(`/${customer.type.toLowerCase()}/my-orders`);
+              }
+              enqueueSnackbar("Transaction status: " + transaction.status, { variant: "success"}); // transaction.status && transaction.id
               handleStatusChange();
+
             })
             .catch((err) => {
               console.error('Capture error:', err);
